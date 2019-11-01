12f7f85610cb30d91609cd82104a9cbfda9f602b
GS-COMI <olaf.mendoza.sc@gmail.com>
2019-11-01 09:15:57 -0600

    Programacion

---
+++ Programacion/JavaScript/js/02-variables.js
@@ -0,0 +1,7 @@
+// VAMOS A TRABAJAR CON VARIABLES
+//UNA VARIABLE ES UN CONTENEDOR DE INFORMACION
+
+var pais = "Mexico";
+var continente = "Amererica";
+
+console.log(pais, continente);
---
+++ Programacion/JavaScript/js/hola-mundo.js
@@ -0,0 +1,5 @@
+/*
+alert("Hola Munfo con JS");
+document.write("Hola mundo desde fichero externo en JS");
+console.log("Muestra esto en la consola");
+console.log(88+4);*/
